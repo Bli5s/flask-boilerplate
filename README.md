524ee6ffcc600a8367839d7442c5fee1cb71736d
Trym Skilleås <trym.skilleas@bdo.no>
2019-03-05 12:38:31 +0100

    ADD: stuff

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # flask-boilerplate
-A no bullshit, no bloat, simple flask boilerplate. Git clone, pip -r install requirements, python server.py.
+A no bullshit, no bloat, simple flask boilerplate. Git clone, pip -r install requirements.txt, python server.py.
---
+++ server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import json
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, jsonify, request, send_file
+
+application = Flask(__name__, static_folder='build', static_url_path='')
+
+if not os.path.isdir('./log'):
+    os.mkdir('./log')
+
+logger = logging.getLogger(__name__)
+consoleHandler = logging.StreamHandler()
+fileHandler = RotatingFileHandler(os.path.join('./log', "my-application-{}.log".format(datetime.datetime.today().isoformat())), maxBytes=10*1024*1024)
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+consoleHandler.setFormatter(formatter)
+consoleHandler.setLevel(logging.WARNING)
+fileHandler.setFormatter(formatter)
+fileHandler.setLevel(logging.DEBUG)
+logger.addHandler(consoleHandler)
+logger.addHandler(fileHandler)
+
+
+"""
+@application.route("/api/data", methods=["POST"])
+def method():
+    try:
+        if true:
+            return jsonify({"result": "yes"})
+        else:
+            raise ValueError("no")
+    except Exception as err:
+        logger.error(err)
+        return jsonify({"error": err}), 400
+"""
+
+@application.route("/", methods=["GET"])
+def root():
+    return application.send_static_file('index.html')
+
+# Run application
+if __name__ == "__main__":
+	application.run()
